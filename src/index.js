f58eaeb3dfd3f3bb08c594ffc6f4c810962e0a60
Hung Nguyen <nmhung.7676@gmail.com>
2022-07-01 21:52:01 +0700

    Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 //HTTP logger
 // app.use(morgan("combined"));
@@ -25,18 +29,8 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources\\views"));
 console.log(path.join(__dirname, "resources\\views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-app.post("/search", (req, res) => {
-  res.send("");
-});
+// Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
