aaa7375b8254bdbbb16090c905d49543b0b80362
Hung Nguyen <nmhung.7676@gmail.com>
2022-07-01 18:30:13 +0700

    POST method

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 //HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan("combined"));
 
 //Template engine
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources\\views'));
-console.log(path.join(__dirname, 'resources\\views'));
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources\\views"));
+console.log(path.join(__dirname, "resources\\views"));
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  res.send("");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
